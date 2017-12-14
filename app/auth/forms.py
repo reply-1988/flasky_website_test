f4c3f422c5a3149d7dd3349f6e79b0843064e7ed
reply-1988 <reply-1988@users.noreply.github.com>
2017-12-15 00:01:54 +0800

    完成注册登陆等功能

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,36 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..modles import User
+
+# 定义登陆与注册表单类
 
 class LoginForm(Form):
+    email = StringField('邮箱', validators=[DataRequired(), Length(1, 64), Email()])
+    password = PasswordField('密码', validators=[DataRequired()])
+    remember_me = BooleanField('保持登陆状态')
+    submit = SubmitField('登陆')
+
+class RegistrationForm(Form):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('keep me login in.')
-    submit = SubmitField('Log in ')
+    username = StringField('Username', validators=[
+        DataRequired(), Length(1, 64), Regexp('^[A-Za-z0-9_.]*$', 0, '用户名必须是字幕，数字，下划线，点，*和$')])
+    password = PasswordField('请输入您的密码', validators=[
+        DataRequired(), EqualTo('password2', message='两次输入的密码必须相同QAQ！')
+    ])
+    password2 = PasswordField('请再一次输入您的密码', validators=[DataRequired()])
+    submit = SubmitField('注册')
+
+    """
+    下面两个是自定义函数，用来验证输入的用户名和邮箱是否重复。
+    在表单类中定义的以validate_ + 字段名的方法，这些方法就和常规的验证函数一起调用。
+    即为自动在validators中运行"""
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('您输入的电子邮箱地址已经存在了。。。')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('您输入的用户名已经存在了。。。')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,11 +1,38 @@
 from flask import render_template, redirect, request, url_for, flash
+
+from app import db
 from . import auth
-from .forms import LoginForm
-from flask_login import login_user
+from .forms import LoginForm, RegistrationForm
+from flask_login import login_user, login_required, logout_user
 from ..modles import User
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('无效的用户名或者密码，请重新登录QAQ。')
     return render_template('auth/login.html', form=form)
+
+@auth.route('/layout')
+@login_required
+def logout():
+    logout_user()
+    flash('您已经退出登陆了QAQ！欢迎再来！')
+    return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    register_form = RegistrationForm()
+    if register_form.validate_on_submit():
+        user = User(username=register_form.username.data,
+        email=register_form.email.data,
+        password=register_form.password.data)
+        db.session.add(user)
+        flash('现在你可以登陆啦😘')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=register_form)
